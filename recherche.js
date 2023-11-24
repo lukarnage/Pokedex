30cbe1f1d6a031da1ca9e0b17e811a3ffe8c240c
Kaat1 <quentin.bigorgne568@gmail.com>
2023-11-24 16:01:09 +0100

    dede

--- recherche.js
+++ recherche.js
@@ -1,46 +1,37 @@
-function search() {
-    // Récupérer la valeur du champ de recherche
-    var searchTerm = document.getElementById("searchInput").value;
+function searchPokemon() {
+  const searchInput = document.getElementById('searchInput');
+  const searchResultsSection = document.getElementById('searchResults');
+  const searchTerm = searchInput.value.trim().toLowerCase();
 
-    // Récupérer la valeur du filtre sélectionné
-    var filter = document.getElementById("filterSelect").value;
-
-    // Simuler une recherche (remplacez cela par votre logique de recherche réelle)
-    var results = performSearch(searchTerm, filter);
-
-    // Afficher les résultats
-    displayResults(results);
+  if (searchTerm === '') {
+      searchResultsSection.innerHTML = '<p>Veuillez entrer un nom de Pokémon.</p>';
+      return;
   }
 
-  function performSearch(term, filter) {
-    // Remplacez cette fonction par votre propre logique de recherche
-    // Pour l'instant, nous retournons des résultats fictifs
-    var fakeData = [
-      { name: "", number: "" },
-      { name: "", number: "" },
-      // Ajoutez d'autres données fictives selon vos besoins
-    ];
+  // Effectuez la recherche sur le serveur en utilisant le terme de recherche
+  fetch(`https://tyradex.vercel.app/api/v1/pokemon?name=${searchTerm}`)
+      .then((response) => response.json())
+      .then((data) => displaySearchResults(data));
+}
 
-    return fakeData.filter(function(item) {
-      // Filtrer les résultats en fonction du filtre sélectionné
-      return item[filter].toLowerCase().includes(term.toLowerCase());
-    });
+function displaySearchResults(results) {
+  const searchResultsSection = document.getElementById('searchResults');
+  searchResultsSection.innerHTML = ''; // Réinitialise les résultats précédents
+
+  if (results.length === 0) {
+      searchResultsSection.innerHTML = '<p>Aucun Pokémon trouvé.</p>';
+      return;
   }
 
-  function displayResults(results) {
-    var resultsContainer = document.getElementById("results");
-
-    // Effacer les résultats précédents
-    resultsContainer.innerHTML = "";
-
-    // Afficher les nouveaux résultats
-    if (results.length === 0) {
-      resultsContainer.innerHTML = "<p>Aucun Pokémon trouvé</p>";
-    } else {
-      var ul = document.createElement("ul");
-      results.forEach(function(result) {
-        var li = document.createElement("li");
-        li.textContent = result.name + " - " + result.number;
-      });
-    }
- }
+  results.forEach(pokemon => {
+      const resultItem = document.createElement('div');
+      resultItem.innerHTML = `
+          <h3>${pokemon.name.fr}</h3>
+          <img src="${pokemon.sprites.regular}" alt="${pokemon.name.fr}">
+          <p>Type: ${pokemon.types.map(type => type.name).join(', ')}</p>
+          <p>Génération: ${pokemon.generation}</p>
+          <!-- Ajoutez d'autres détails du Pokémon selon vos besoins -->
+      `;
+      searchResultsSection.appendChild(resultItem);
+  });
+}
